63b11930414537b6aad1e46165095d452f2d44ae
s19003 <s19003@std-it.college.ac.jp>
2021-06-01 21:45:04 +0900

    06/11

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-import express from 'express'
-const app = express()
-
 function onButtonClick () {
   alert('(*´∀｀*)<Hello World!!')
 }
-
-app.get('/index', (req, res) => {
-  console.log('Get')
-})
