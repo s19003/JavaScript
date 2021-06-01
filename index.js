b777c776c94448ac58d5ccdcf87667d1d7cb125b
s19003 <s19003@std-it.college.ac.jp>
2021-06-01 21:33:47 +0900

    06/01

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const express = require('express')
+const app = express()
+
 function onButtonClick () {
   alert('(*´∀｀*)<Hello World!!')
 }
+
+app.get('/index', (req, res) => {
+  res.write('Hello World!!')
+})
