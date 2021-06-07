24570442c257b657093ccae2df6258fd8eac528c
s19003 <s19003@std-it.college.ac.jp>
2021-06-07 22:28:17 +0900

    06/07

---
+++ Learning/codeRecipe/013/01.js
@@ -0,0 +1,19 @@
+/*****
+一定時間後に処理を行いたい
+*****/
+
+console.log(new Date().toLocaleDateString())
+
+const greet = () => {
+  console.log('Hello')
+}
+setTimeout(greet, 3000) // -- 3秒後に実行
+
+setTimeout(() => {
+  console.log('World!!')
+}, 5000) // -- 5秒後に実行
+
+setTimeout(() => {
+  const date = new Date().toLocaleTimeString()
+  console.log(date)
+}, 7000) // -- 7秒後に実行
