1d1a07dfc69f486197d64cee2e5591e91666390d
s19003 <s19003@std-it.college.ac.jp>
2021-11-19 22:29:57 +0900

    11/19

--- threejs/index.js
+++ threejs/index.js
@@ -5,11 +5,11 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
+app.get('/01', (req, res) => {
+  res.sendFile(__dirname + 'public/01/index.html')
 })
 
 const port = 3000
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
+  console.log(`http://localhost:${3000}/01`)
 })
